7260e3765ce02fa41e3bade87d2fdf9717a40600
asanketghugare <sanketghugare0103@gmail.com>
2025-03-07 00:39:03 +0530

    new substraction feature added

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,11 +3,18 @@ class Calculator {
     public int add(int a , int b){
         return a+b;
     }
+
+    public int sub(int a , int b){
+        return a-b;
+    }
 }
-class CalcualatorTest{
+    
+
+class CalculatorTest{
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Result : "+ calculator.add(10,20));
+        System.out.println("Result : "+ calculator.sub(30,10));
     }
 }
